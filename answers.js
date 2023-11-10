97b5f26e39170710148855cfdf2a18573f3f41cb
RachelY <yang.yijing@outlook.com>
2023-11-09 23:51:33 -0500

    where is Waldo answered

--- answers.js
+++ answers.js
@@ -110,6 +110,22 @@ console.log(favMovies.indexOf('Fast and Furious'))
 
 ////////////////////////////////
 // Where is Waldo
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", ["Baked Goods", "Waldo"]]];
+//Remove Eggbert (hint look at the slice/splice method(s))
+whereIsWaldo.splice(1,1)
+console.log(whereIsWaldo)
+//Change "Neff" to "No One"
+const neffIndex = whereIsWaldo[1].indexOf("Neff");
+// Check if "Neff" is found and replace it with "No One"
+if (neffIndex !== -1) {
+    whereIsWaldo[1][neffIndex] = "No One";
+}
+console.log(whereIsWaldo);
+// Access and log the element "Waldo"
+const waldoElement = whereIsWaldo[2][1][1];
+console.log(waldoElement);
 ////////////////////////////////
 
 
